792600dc775973ecc9efa06d6375faf01306d8b1
Nirvik Gill <majorblazer69420@gmail.com>
2026-04-15 23:57:25 -0400

    fixed something

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,7 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  allowedDevOrigins: ["10.0.0.248", "192.168.*"],
 };
 
 export default nextConfig;
